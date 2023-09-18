8553b4fc25f5ef58340e83476fd3a6592e004944
chidicolly11@gmail.com <chidicolly11@gmail.com>
2023-09-18 06:15:40 +0100

    magneto bank applicationg

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -34,9 +34,9 @@ export default function Login() {
 };
 
 
-// const handleButtonClick = () => {
-//   alert('logged in successfully');
-// };
+const handleButtonClick = () => {
+  alert('logged in successfully');
+};
   return (
     <div className='flex justify-center mt-[4rem]'>
         <section>
@@ -57,7 +57,7 @@ export default function Login() {
                 </div>
             </div>
             <div className='mt-9'>
-                <button className='w-[29.8rem] h-[3.6rem] bg-red-600 rounded text-white' ><Link to='/home'>Log in</Link></button>
+                <button  onClick={handleButtonClick} className='w-[29.8rem] h-[3.6rem] bg-red-600 rounded text-white' ><Link to='/home'>Log in</Link></button>
                 <div>
                     <p className='font-thin ml-3'>For further support, you may visit the Help Center or contact our <br></br>customer service team.</p>
                 </div>
